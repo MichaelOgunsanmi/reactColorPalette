5c2fb65a987fa42ef07f6bdbfadbff476a7d4439
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-05 23:15:59 +0100

    Cleaned up the src directory, added relevant folders and added the Palette container

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Palette = () => {
+    return (
+        <div className={'Palette'}>
+            {/*Navbar Here*/}
+            <div className="Palette-colors">PALETTE</div>
+            {/*Footer eventually*/}
+        </div>
+    );
+};
+
+export default Palette;

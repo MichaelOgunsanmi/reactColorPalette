ce4e5d8c7a79e3e3729a9e7055c11f6381bb99e5
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-10 02:30:13 +0100

    Configured the app to work with react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-import seedPalettes from "./utils/seedPalettes";
+import {Route, Switch} from "react-router-dom";
+
 import Palette from "./Palette";
+
+import seedPalettes from "./utils/seedPalettes";
 import {generatePalette} from "./utils/colorHelpers";
 
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedPalettes[4])} />
-    </div>
+      <Switch>
+          <Route exact path={'/'} render={() => <h1>HELLO FROM INDEX</h1>}/>
+          <Route exact path={'/palette/:paletteId'} render={() => <h1>HELLO FROM Palette</h1>}/>
+      </Switch>
+    // <div>
+    //   <Palette palette={generatePalette(seedPalettes[4])} />
+    // </div>
   );
 }
 

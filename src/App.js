547d1b702659cfee26f9b81bc83f645e5885b7ec
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-10 02:45:23 +0100

    Fixed palette route with error handling for invalid routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 import Palette from "./Palette";
 
@@ -9,15 +9,23 @@ import {generatePalette} from "./utils/colorHelpers";
 
 
 function App() {
-  return (
+    const getPalette = (paletteId) => {
+        const seedPalette = seedPalettes.find(palette => palette.id.toLowerCase() === paletteId.toLowerCase());
+
+        if (!seedPalette) return <Redirect to={'/'}/>;
+
+        return <Palette palette={generatePalette(seedPalette)}/>;
+    };
+
+    return (
       <Switch>
           <Route exact path={'/'} render={() => <h1>HELLO FROM INDEX</h1>}/>
-          <Route exact path={'/palette/:paletteId'} render={() => <h1>HELLO FROM Palette</h1>}/>
+          <Route exact path={'/palette/:paletteId'} render={(routeProps) => getPalette(routeProps.match.params.paletteId)}/>
       </Switch>
     // <div>
     //   <Palette palette={generatePalette(seedPalettes[4])} />
     // </div>
-  );
+    );
 }
 
 export default App;

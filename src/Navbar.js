2a122588920cde359aba79b79efaa548fd735008
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-10 01:09:40 +0100

    Added the snackbar component and styled the select container

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,20 +4,29 @@ import React, {useState} from 'react';
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 
 import "rc-slider/assets/index.css"
 import "./Navbar.css";
 
 
+
 const Navbar = (props) => {
     const [format, setFormat] = useState("hex");
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+
 
     const handleFormatChange = (event) => {
         const selectedFormat = event.target.value;
         setFormat(selectedFormat);
+        setOpenSnackbar(true);
         props.changeColorFormat(selectedFormat);
     };
+
+    const handleCloseSnackbar = () => {setOpenSnackbar(false)};
 
 
     return (
@@ -44,6 +53,25 @@ const Navbar = (props) => {
                     <MenuItem value="rgba">RGBA </MenuItem>
                 </Select>
             </div>
+            <Snackbar
+                anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+                open={openSnackbar}
+                autoHideDuration={3000}
+                message={<span id="message-id">Color format changed to {format.toUpperCase()}</span>}
+                ContentProps={{ "aria-describedby" : "message-id"}}
+                onClose={handleCloseSnackbar}
+                action={[
+                    <IconButton
+                        onClick={handleCloseSnackbar}
+                        color={'inherit'}
+                        key={'close'}
+                        aria-label={'close'}
+                    >
+                        <CloseIcon/>
+                    </IconButton>
+                ]}
+
+            />
         </header>
     );
 };

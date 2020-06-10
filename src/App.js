30e066940257f2db3aa29b34ab738edc05bafbf9
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-10 17:55:28 +0100

    Added route for rendering a single color box

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import {Redirect, Route, Switch} from "react-router-dom";
 
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
+import SingleColorBox from "./SingleColorBox";
 
 import seedPalettes from "./utils/seedPalettes";
 import {generatePalette} from "./utils/colorHelpers";
+
 
 
 
@@ -23,10 +25,8 @@ function App() {
       <Switch>
           <Route exact path={'/'} render={(routeProps) => <PaletteList palettes={seedPalettes} {...routeProps}/>}/>
           <Route exact path={'/palette/:paletteId'} render={(routeProps) => getPalette(routeProps.match.params.paletteId)}/>
+          <Route exact path={'/palette/:paletteId/:colorId'} render={() => <SingleColorBox/>}/>
       </Switch>
-    // <div>
-    //   <Palette palette={generatePalette(seedPalettes[4])} />
-    // </div>
     );
 }
 
---
+++ src/SingleColorBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleColorBox = (props) => {
+    return (
+        <div>
+           Rendering
+        </div>
+    );
+};
+
+export default SingleColorBox;

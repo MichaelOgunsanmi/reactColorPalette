6a08e9bdc1dcd5051326584322667d8fbdea249c
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-10 07:37:57 +0100

    Added the MiniPalette component with basic styling

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import {Link} from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 
 const PaletteList = (props) => {
     const {palettes} = props;
 
-    const paletteList = palettes.map( palette => <h4><Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link> </h4>);
+    const paletteList = palettes.map( palette => <MiniPalette {...palette}/>);
+
+    // const helper = <h4><Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link> </h4>;
 
     return (
         <div>

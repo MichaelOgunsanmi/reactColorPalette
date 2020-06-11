8ef2ed9b436f6b82ef87c8c9957cfca9cd812089
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-11 16:28:37 +0100

    Added the go back buttton for a SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -2,25 +2,29 @@ import React, {useState} from 'react';
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import {Link} from "react-router-dom";
 
 const SingleColorPalette = (props) => {
-    const {colors, paletteName, emoji} = props.palette;
+    const {colors, paletteName, emoji, id} = props.palette;
     const [format, setFormat] = useState("hex");
 
     const changeColorFormat = selectedFormat => {setFormat(selectedFormat)};
 
     const colorBoxes = colors.map( color => <ColorBox
-        key={color.id}
+        key={color.name}
         name={color.name}
         backgroundColor={color[format]}
         showLink={false}
     />);
 
     return (
-        <div className={'Palette'}>
+        <div className={'SingleColorPalette Palette'}>
             <Navbar changeColorFormat={changeColorFormat} showSlider={false}/>
             <div className={'Palette-colors'}>
                 {colorBoxes}
+                <div className="go-back ColorBox">
+                    <Link to={`/palette/${id}`} className={'back-button'}>GO BACK</Link>
+                </div>
             </div>
             <Footer paletteName={paletteName} emoji={emoji}/>
         </div>

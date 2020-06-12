233426dbf6b4650020a4914d30071657fbd35625
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-12 20:24:34 +0100

    Refactored the SingleColorPalette styling

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -6,48 +6,9 @@ import {withStyles} from "@material-ui/styles";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import chroma from "chroma-js";
 
+import styles from "./styles/SingleColorPaletteStyles";
 
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-    },
-    colors: {
-        height: "90%",
-    },
-    goBack: {
-        width: "20%",
-        height: "50%",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        margin: "0 auto -3.5px",
-        opacity: 1,
-        backgroundColor: "black",
-        "& a": {
-            color: "white",
-            width: "100px",
-            height: "30px",
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            backgroundColor: "rgba(255, 255, 255, 0.3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            textTransform: "uppercase",
-            textDecoration: "none",
-            border: "none",
-        }
-    }
-};
 
 const SingleColorPalette = (props) => {
     const {colors, paletteName, emoji, id} = props.palette;

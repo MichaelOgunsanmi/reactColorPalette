2c1dbf1d3fd767cd19fdfffc9e19f8d5d563b110
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-06-16 10:21:51 +0100

    Setup the emoji picker

--- src/NewPaletteMetaForm.js
+++ src/NewPaletteMetaForm.js
@@ -6,6 +6,8 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {TextValidator, ValidatorForm} from "react-material-ui-form-validator";
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 
 const NewPaletteMetaForm = (props) => {
@@ -50,6 +52,7 @@ const NewPaletteMetaForm = (props) => {
                         Save Palette
                     </Button>
                 </DialogActions>
+                <Picker/>
             </ValidatorForm>
         </Dialog>
     );
